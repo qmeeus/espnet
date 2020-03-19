6a4ea15d88e89ed1c30701223920c6be7a1586d0
Quentin Meeus <qmeeus@.esat.kuleuven.be>
2020-03-19 12:40:49 +0100

    merge conflict

--- egs/cgn/asr1/run.sh
+++ egs/cgn/asr1/run.sh
@@ -38,7 +38,7 @@ if [ ${stage} -le 1 ]; then
     echo "stage 1: Feature Generation"
     fbankdir=fbank
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
-    for x in ${train_dev} ${train_set}; do
+    for x in ${train_set} ${train_dev}; do
         steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 8 --write_utt2num_frames true \
             data/${x} exp/make_fbank/${x} ${fbankdir}
         utils/fix_data_dir.sh data/${x}
