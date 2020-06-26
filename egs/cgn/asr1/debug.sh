595766ada2bb57473c55de9fc58c4388b78c83ed
Quentin Meeus <qmeeus@gmail.com>
2020-06-26 09:55:48 +0200

    debug script for trfs

---
+++ egs/cgn/asr1/debug.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+./eval_w2v.sh \
+  --ngpu 0 \
+  --train_config conf/train/transformer_12_6_2048_512_8_do.1.yaml \
+  --target word \
+  --resume exp/train_word_vectors_mono/transformer_12_6_2048_512_8_do.1/dec-only/results/model.loss.best \
+  --tag debug \
+  --output_dir exp/debug/w2v/evaluate
+
+#./train.sh \
+#  --ngpu 1 \
+#  --target word \
+#  --output_dir exp/debug/w2v/train \
+#  --train_config conf/train/transformer_12_6_2048_256_4_a.3_do.1.yaml \
+#  --dataset_tag o \
+#  --tag debug
+
+#./train.sh \
+#  --ngpu 0 \
+#  --target word \
+#  --output_dir exp/debug/w2v/train \
+#  --train_config conf/train/lstmp_6_3_800_a.4_do.1.1_wd5.yaml \
+#  --dataset_tag mono \
+#  --tag debug
+
