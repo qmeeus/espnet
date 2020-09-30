4a4742e2f566bbf4c0621f5ccf1d4403d3d3d44a
Quentin Meeus <qmeeus@gmail.com>
2020-09-30 12:32:30 +0200

    train_hybrid script changes

--- egs/cgn/asr1/train_hybrid.sh
+++ egs/cgn/asr1/train_hybrid.sh
@@ -7,16 +7,16 @@ outdir=$1
 . setup.sh
 . path.sh
 
-# asr_model=exp/train_unigram_5000_mono/transformer_12_6_2048_512_8_a.4_do.1/train/results/model.loss.best
-# hybrid_model=exp/train_hybrid_mono/transformer_12_6_2048_512_8_do.1/dec-only/results/model.loss.best
-# teacher_model=distilbert-base-multilingual-cased
+asr_model=exp/train_unigram_5000_mono/transformer_12_6_2048_512_8_a.4_do.1/train/results/model.loss.best
+hybrid_model=exp/train_hybrid_mono/transformer_12_6_2048_512_8_asched_do.1/dec-only/results/model.loss.best
+teacher_model=distilbert-base-multilingual-cased
 
-asr_model=exp/train_unigram_5000_all/transformer_12_6_2048_512_8_a.4_do.1/train/results/model.loss.best
-teacher_model=examples/language-modeling/output/distilbert/checkpoint-134000
+#asr_model=exp/train_unigram_5000_all/transformer_12_6_2048_512_8_a.4_do.1/train/results/model.loss.best
+#teacher_model=examples/language-modeling/output/distilbert/checkpoint-134000
 
 w2v_train.py \
   --v1 \
-  --config conf/train/transformer_12_6_2048_512_8_a.1e-5_do.1.yaml \
+  --config conf/train/transformer_12_6_2048_512_8_asched_do.1.yaml \
   --ngpu 1 \
   --backend pytorch \
   --outdir $outdir/results \
@@ -31,6 +31,8 @@ w2v_train.py \
   --train-json dump/CGN_train/deltafalse/data_unigram_5000.mono.json \
   --valid-json dump/CGN_valid/deltafalse/data_unigram_5000.mono.json \
   --teacher-model=$teacher_model \
-  --enc-init $asr_model #\
-#  --dec-init $hybrid_model \
+  --enc-init $hybrid_model \
+  --dec-init $hybrid_model
+#  --enc-init $asr_model \
+#  --freeze-encoder -1
 
