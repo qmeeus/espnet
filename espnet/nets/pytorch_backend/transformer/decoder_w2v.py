1381187482bdd2ea6bd813bc574ca8ae05b2d714
Quentin Meeus <qmeeus@gmail.com>
2020-10-09 16:22:20 +0200

    old changes decoder_w2v compat mode

--- espnet/nets/pytorch_backend/transformer/decoder_w2v.py
+++ espnet/nets/pytorch_backend/transformer/decoder_w2v.py
@@ -8,15 +8,22 @@ class Decoder(BaseDecoder):
 
     def get_input_layer(self, input_layer,
                         dropout_rate=None,
-                        positional_dropout_rate=.1, 
+                        positional_dropout_rate=.1,
                         pos_enc_class=PositionalEncoding):
-        
+
+        if False:
+            return self.get_input_layer_compat(
+                input_layer,
+                positional_dropout_rate=positional_dropout_rate,
+                pos_enc_class=pos_enc_class
+            )
+
         layer = nn.Sequential()
 
         if isinstance(input_layer, nn.Module):
             layer.add_module("input_layer", input_layer)
 
-        if self.output_dim != self.attention_dim:
+        if self.output_dim != self.attention_dim or input_layer == "linear":
             layer.add_module("linear", nn.Linear(self.output_dim, self.attention_dim))
             layer.add_module("layer_norm", nn.LayerNorm(self.attention_dim))
             layer.add_module("dropout", nn.Dropout(dropout_rate))
@@ -25,3 +32,14 @@ class Decoder(BaseDecoder):
         layer.add_module("positional_encoding", pos_enc_class(self.attention_dim, positional_dropout_rate))
 
         return layer
+
+    def get_input_layer_compat(self, input_layer,
+                        dropout_rate=None,
+                        positional_dropout_rate=.1,
+                        pos_enc_class=PositionalEncoding):
+
+        return nn.Sequential(
+            input_layer,
+            nn.Linear(self.output_dim, self.attention_dim),
+            pos_enc_class(self.attention_dim, positional_dropout_rate)
+        )
