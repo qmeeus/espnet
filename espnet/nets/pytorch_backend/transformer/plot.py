237fdb1cb15caa84eb5a168feee642a364032919
Quentin Meeus <qmeeus@gmail.com>
2020-11-30 10:58:51 +0100

    fix bug in attn plot with wrong number of ticks vs ticklabels in mpl 3.3

--- espnet/nets/pytorch_backend/transformer/plot.py
+++ espnet/nets/pytorch_backend/transformer/plot.py
@@ -5,11 +5,10 @@
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 
 import logging
-
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
-from espnet.asr import asr_utils
+from espnet.asr.asr_utils import PlotAttentionReport
 
 
 def _plot_and_save_attention(att_w, filename, xtokens=None, ytokens=None):
@@ -21,26 +20,26 @@ def _plot_and_save_attention(att_w, filename, xtokens=None, ytokens=None):
     if not os.path.exists(d):
         os.makedirs(d)
     w, h = plt.figaspect(1.0 / len(att_w))
-    fig = plt.Figure(figsize=(w * 2, h * 2))
+    fig = plt.figure(figsize=(w * 2, h * 2))
     axes = fig.subplots(1, len(att_w))
     if len(att_w) == 1:
         axes = [axes]
     for ax, aw in zip(axes, att_w):
         # plt.subplot(1, len(att_w), h)
-        ax.imshow(aw.astype(numpy.float32), aspect="auto")
+        ax.imshow(aw.astype(np.float32), aspect="auto")
         ax.set_xlabel("Input")
         ax.set_ylabel("Output")
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         ax.yaxis.set_major_locator(MaxNLocator(integer=True))
         # Labels for major ticks
         if xtokens is not None:
-            ax.set_xticks(numpy.linspace(0, len(xtokens) - 1, len(xtokens)))
-            ax.set_xticks(numpy.linspace(0, len(xtokens) - 1, 1), minor=True)
-            ax.set_xticklabels(xtokens + [""], rotation=40)
+            ax.set_xticks(np.linspace(0, len(xtokens) - 1, len(xtokens)))
+            ax.set_xticks(np.linspace(0, len(xtokens) - 1, 1), minor=True)
+            ax.set_xticklabels(xtokens, rotation=40)
         if ytokens is not None:
-            ax.set_yticks(numpy.linspace(0, len(ytokens) - 1, len(ytokens)))
-            ax.set_yticks(numpy.linspace(0, len(ytokens) - 1, 1), minor=True)
-            ax.set_yticklabels(ytokens + [""])
+            ax.set_yticks(np.linspace(0, len(ytokens) - 1, len(ytokens)))
+            ax.set_yticks(np.linspace(0, len(ytokens) - 1, 1), minor=True)
+            ax.set_yticklabels(ytokens)
     fig.tight_layout()
     return fig
 
@@ -114,7 +113,7 @@ def plot_multi_head_attention(
             savefn(fig, filename)
 
 
-class PlotAttentionReport(asr_utils.PlotAttentionReport):
+class PlotAttentionReport(PlotAttentionReport):
     def plotfn(self, *args, **kwargs):
         kwargs["ikey"] = self.ikey
         kwargs["iaxis"] = self.iaxis
